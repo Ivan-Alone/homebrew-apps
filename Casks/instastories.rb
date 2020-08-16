a3461cd6a0bea4e5721e30f17d8d387f01e5f346
Ivan <ivan-my-mail@yandex.com>
2020-08-16 15:53:25 +0300

    Update instastories.rb

--- Casks/instastories.rb
+++ Casks/instastories.rb
@@ -1,6 +1,6 @@
 cask "instastories" do
   version "1.0.0"
-  sha256 "e2fd677dabe30b50c35eb68fbef6e27bb3a571f37e36b07dd4695a734fdbd3ad"
+  sha256 "752811717ae77e360aa61870d4d54c6c59ccc667784089240d9a24e28b26ed9c"
 
   url "https://raw.githubusercontent.com/Ivan-Alone/InstaStories-Saver/master/macos-app/InstaStories-Saver-v#{version}.zip"
   name "InstaStories Saver"
