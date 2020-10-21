487b20f5028621b807f62d37de55791398526766
Ivan <ivan-my-mail@yandex.com>
2020-10-21 21:38:01 +0300

    Updated application to v1.0.1 (enc_password fix)

--- Casks/instastories.rb
+++ Casks/instastories.rb
@@ -1,6 +1,6 @@
 cask "instastories" do
-  version "1.0.0"
-  sha256 "752811717ae77e360aa61870d4d54c6c59ccc667784089240d9a24e28b26ed9c"
+  version "1.0.1"
+  sha256 "397d93ec9795430365605ac632672102c5a686c04edb467dc19638f3e3856083"
 
   url "https://raw.githubusercontent.com/Ivan-Alone/InstaStories-Saver/master/macos-app/InstaStories-Saver-v#{version}.zip"
   name "InstaStories Saver"
