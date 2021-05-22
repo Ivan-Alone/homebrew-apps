0bc58658082bf3cb6c172dcefceea4e95533bcde
Ivan <ivan-my-mail@yandex.com>
2021-05-22 14:53:57 +0300

    Updated for working

--- Formula/efi-oc.rb
+++ Formula/efi-oc.rb
@@ -1,11 +1,11 @@
 class EfiOc < Formula
   desc "OpenCore EFI automount terminal tool"
   url "https://github.com/Ivan-Alone/homebrew-apps/raw/master/efi-oc.tar.xz"
-  sha256 "391b4d30fe049b03421ad98386bb10af16922f49ffa11f0519983df023498c8d"
+  sha256 "16118e85b30a3906c478aae684779bfe4526484662a982f1fc970187817626cf"
   license "GPL-3.0-or-later"
-  version "0.1.3"
+  version "0.1.4"
   
   def install
-    bin.install "efi.sh" => "efi"
+    bin.install "efi.php" => "efi"
   end
 end
